44537c021cfa60ed5eed55cc9f940e83c5dd5d54
batman <pgrisales@unal.edu.co>
2022-09-26 21:37:43 -0500

    docker-compose updated

--- UNcademy_acadRec_ms/config/db.py
+++ UNcademy_acadRec_ms/config/db.py
@@ -1,7 +1,13 @@
 from pymongo import MongoClient
+import os
 
-#conn = MongoClient('uncademy_acadrec_db',27018)
-conn = MongoClient('mongodb://uncademy_acadrec_db:27018/')
+#conn = MongoClient('uncademy_acadrec_db', 27088)
+#conn = MongoClient('mongodb://uncademy_acadrec_db/27017/')
+#conn = MongoClient('mongodb://uncademy_acadrec_db:27088')
+#conn = MongoClient('mongodb://b8cc1cbfbe39_UNcademy_acadRec_db:27088')
+#conn = MongoClient('mongodb://db:27017/')
+#dbURL = os.environ["DB_URL"]
+conn = MongoClient(os.environ['DB_URL'])
 #conn = MongoClient()
 
 db = conn["AcademicRecords"]
